913010abf9875c569c7576f46c2a434d18f084c8
Adam Lynam <adam@lynam.co.nz>
2022-11-02 17:44:09 +1300

    Adding the AWS profile to use as a variable

--- infrustructure/variables.tf
+++ infrustructure/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "ap-southeast-2"
 }
+
+variable "aws_profile" {
+  description = "The AWS profile to create things with."
+  default     = "SREAssessment.Infrustructure"
+}
