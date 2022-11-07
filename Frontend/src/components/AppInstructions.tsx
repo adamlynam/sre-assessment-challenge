891e3207676f35f28c195e92f11ed0647af67c2f
Adam Lynam <adam@lynam.co.nz>
2022-11-07 22:00:03 +1300

    Pushing a small change to trigger the pipeline

--- Frontend/src/components/AppInstructions.tsx
+++ Frontend/src/components/AppInstructions.tsx
@@ -7,7 +7,7 @@ export const AppInstructions = () => (
         <Alert.Heading>Todo List App</Alert.Heading>
         Welcome to the ClearPoint SRE technical test.
         <br />
-        This is Adam Lynam's solution.
+        This is Adam Lynam's solution. It runs on Fargate instances on AWS Elastic Container Services and has Continous Deployment configured.
         <br />
 
       </Alert>
